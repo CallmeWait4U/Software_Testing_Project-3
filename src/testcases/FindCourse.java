ca5ff45ff00d4214885d8de62c6ed90f3988e2a7
DucHuy2801 <theflash28012002@gmail.com>
2023-12-05 00:26:36 +0700

    find course

---
+++ src/testcases/FindCourse.java
@@ -0,0 +1,41 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import base.DriverInstance;
+
+public class FindCourse extends DriverInstance{
+
+    @Test
+    public void tc_findcourse_1() throws Exception {
+        driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("moodle");
+		driver.findElement(By.id("loginbtn")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        WebElement searchInput = driver.findElement(By.id("nav-search-query"));
+        searchInput.sendKeys("Moodle and Mountaineering");
+        searchInput.submit()
+    }
+
+    @Test
+    public void tc_findcourse_2() throws Exception {
+        driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("moodle");
+		driver.findElement(By.id("loginbtn")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        WebElement searchInput = driver.findElement(By.id("nav-search-query"));
+        searchInput.sendKeys("Kiem tra phan mem");
+        searchInput.submit()
+    }
+
+}
