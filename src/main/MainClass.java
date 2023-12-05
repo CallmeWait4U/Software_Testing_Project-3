cc3556c3b48ab1ff72ce2557dc12086bf43aa42a
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2023-12-06 03:28:23 +0700

    last commit

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -10,11 +10,9 @@ import usecases.MultiLanguageUsecase;
 import usecases.ResponseTimeSearchingForumsUsecase;
 import usecases.SubmitTextUsecase;
 import usecases.UploadFileUsecase;
-import usecases.FindCourseUsecase;
 import usecases.AddTopicToForumUsecase;
 import usecases.SendMessageUsecase;
 import usecases.SortFilterActivitiesUsecase;
-import usecases.UploadFileUsecase;
 import usecases.ViewListOfActivitiesUsecase;
 import usecases.LoginUsecase;
 import usecases.MessageUsecase;
@@ -25,69 +23,69 @@ public class MainClass {
 	public static void main(String args[]) throws Exception {
 		// Usecase: Upload File
 		UploadFileUsecase uploadfile = new UploadFileUsecase();
-		// uploadfile.run_testcase();
+		uploadfile.run_testcase();
 		
 		// Usecase: Grading
 		GradingUsecase grading = new GradingUsecase();
-		// grading.run_testcase();
+		grading.run_testcase();
 		
 		// Usecase: Add a Category
 		AddCategoryUsecase addCategory = new AddCategoryUsecase();
-//		addCategory.run_testcase();
+		addCategory.run_testcase();
 		
 		// Usecase: Response Time Searching Forums
 		ResponseTimeSearchingForumsUsecase responseTimeSearchingForums = new ResponseTimeSearchingForumsUsecase();
-//		responseTimeSearchingForums.run_testcase();
+		responseTimeSearchingForums.run_testcase();
 
 		// Usecase: Find course
 		FindCourseUsecase findCourse = new FindCourseUsecase();
-		// findCourse.run_testcase();
+		findCourse.run_testcase();
 
 
 		// Usecase: Add topic to forum
 		AddTopicToForumUsecase addTopicToForum = new AddTopicToForumUsecase();
-		// addTopicToForum.run_testcase();
+		addTopicToForum.run_testcase();
 
 		// Usecase: Add a LTI External Tool
 		AddALTIExternalToolUsecase addALTIExternalTool = new AddALTIExternalToolUsecase();
-		// addALTIExternalTool.run_testcase();
+		addALTIExternalTool.run_testcase();
 
 		// Usecase: Submit text
 		SubmitTextUsecase submitText = new SubmitTextUsecase();
-		// submitText.run_testcase();
+		submitText.run_testcase();
 
 		// Usecase: Response Time Searching Student Grade
 		ResponseTimeSearchingStudentGradeUsecase responseTimeSearchingStudentGrade = new ResponseTimeSearchingStudentGradeUsecase();
-		// responseTimeSearchingStudentGrade.run_testcase();
+		responseTimeSearchingStudentGrade.run_testcase();
 
 		// Usecase: Edit profile
 		EditProfileUsecase editProfile = new EditProfileUsecase();
-		// editProfile.run_testcase();
+		editProfile.run_testcase();
 
 		// Usecase: Sort filter activities
 		SortFilterActivitiesUsecase sortFilterActivities = new SortFilterActivitiesUsecase();
-		// sortFilterActivities.run_testcase();
+		sortFilterActivities.run_testcase();
 
 		// Usecase: View list of activities
 		ViewListOfActivitiesUsecase viewListOfActivities = new ViewListOfActivitiesUsecase();
-//		 viewListOfActivities.run_testcase();
+		 viewListOfActivities.run_testcase();
 
 		// Usecase: Multi language
 		MultiLanguageUsecase multilanguage = new MultiLanguageUsecase();
-		// multilanguage.run_testcase();
+		multilanguage.run_testcase();
 
 		// Usecase: Send message
 		SendMessageUsecase sendmessage = new SendMessageUsecase();
-		// sendmessage.run_testcase();
+		sendmessage.run_testcase();
 		
 		LoginUsecase login = new LoginUsecase();
-//		login.run_testcase();
+		login.run_testcase();
 		
 		MessageUsecase message = new MessageUsecase();
-		//message.run_testcase();
+		message.run_testcase();
 		
 		ResponseTimeSubmitingAQuizUsescase responseTimeSubmitingAQuizUsescase = new ResponseTimeSubmitingAQuizUsescase();
-		//responseTimeSubmitingAQuizUsescase.run_testcase();
+		responseTimeSubmitingAQuizUsescase.run_testcase();
 
 	}
 }
--- src/testcases/EditProfile.java
+++ src/testcases/EditProfile.java
@@ -3,8 +3,6 @@ package testcases;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
